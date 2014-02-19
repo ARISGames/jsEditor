dd9aaecac92a7fc2343a50ed61269a109d40899c
Kevin Alford <zeroeth@pixelflow.org>
2014-02-18 20:56:36 -0600

    Fixed syntax

--- scripts/models/quest.js
+++ scripts/models/quest.js
@@ -41,25 +41,25 @@ define([
 		],
 
 		defaults: {
-			active_icon_media_id: 0
-			active_media_id: 0
-			active_notif_show_dismiss: 1
-			active_notification_media_id: 0
-			complete_full_screen_notify: 1
-			complete_go_function: "NONE"
-			complete_icon_media_id: 0
-			complete_media_id: 0
-			complete_notif_go_function: "NONE"
-			complete_notif_show_dismiss: "1"
-			complete_notification_media_id: 0
-			description: ""
-			description_notification: ""
-			full_screen_notify: 1
-			go_function: "NONE"
-			name: "New Quest"
-			notif_go_function: "NONE"
-			sort_index: 0
-			text_when_complete: ""
+			active_icon_media_id: 0,
+			active_media_id: 0,
+			active_notif_show_dismiss: 1,
+			active_notification_media_id: 0,
+			complete_full_screen_notify: 1,
+			complete_go_function: "NONE",
+			complete_icon_media_id: 0,
+			complete_media_id: 0,
+			complete_notif_go_function: "NONE",
+			complete_notif_show_dismiss: "1",
+			complete_notification_media_id: 0,
+			description: "",
+			description_notification: "",
+			full_screen_notify: 1,
+			go_function: "NONE",
+			name: "New Quest",
+			notif_go_function: "NONE",
+			sort_index: 0,
+			text_when_complete: "",
 			text_when_complete_notification: ""
 		}
 	});
