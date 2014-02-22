1a8865bbb6de1f74f8e46eb3749509c62b80e8f0
Kevin Alford <zeroeth@pixelflow.org>
2014-02-22 15:05:15 -0600

    Added google map to render locations and center on their coordinates

--- scripts/views/locations.js
+++ scripts/views/locations.js
@@ -11,10 +11,8 @@ define([
 		template: _.template(Template),
 
 		itemView: LocationItemView,
-
-	    // Bootstrap wrapper
-		tagName: "table",
-		className: "table",
+		itemViewContainer: ".itemViewContainer",
+	
 
 		events: {
 			"click .new": "onClickNew"
@@ -23,6 +21,37 @@ define([
 
 		onClickNew: function() {
 			Backbone.history.navigate("#games/"+this.collection.parent.get('game_id')+"/locations/new", {trigger: true});
+		},
+
+
+		onRender: function() {
+			// Render Map
+			var element = this.$el.find('.map-canvas').get(0);
+
+			var map_options = {
+				zoom: 8,
+				center: new google.maps.LatLng(-34.397, 150.644)
+			};
+			var map = new google.maps.Map(element, map_options);
+			var boundary = new google.maps.LatLngBounds();
+
+			// Add Locations to map
+			_.each(this.collection.models, function(location) {
+				var location_position = new google.maps.LatLng(location.get('latitude'), location.get('longitude'));
+
+				// Extend boundary with all locations
+				boundary.extend(location_position);
+
+				var marker = new google.maps.Marker({
+					position: location_position,
+					map: map,
+					title: location.get('name')
+				});
+			});
+
+			// Fit map to all locations
+			map.setCenter(boundary.getCenter());
+			map.fitBounds(boundary);
 		}
 	});
 });
