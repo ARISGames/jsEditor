3f7bb645c11c5732a1dc7f970a4723a2678086ec
Kevin Alford <zeroeth@pixelflow.org>
2014-05-24 16:58:05 -0500

    Show dialog name on scene

--- scripts/views/scene_instance_trigger.js
+++ scripts/views/scene_instance_trigger.js
@@ -2,8 +2,10 @@ define([
 	'underscore',
 	'backbone',
 	'text!../../templates/scene_instance_trigger.tpl',
+	'models/instance',
+	'models/dialog',
 	'vent'
-], function(_, Backbone, Template, vent) {
+], function(_, Backbone, Template, Instance, Dialog, vent) {
 
 	return Backbone.Marionette.CompositeView.extend({
 		template: _.template(Template),
@@ -13,8 +15,29 @@ define([
 
 		templateHelpers: function() {
 			return {
-				object_name: this.model.get("title")
+				object_name: this.object_name
 			}
+		},
+
+		initialize: function() {
+			var view = this;
+
+			view.object_name = "";
+
+			view.instance = new Instance({instance_id: view.model.get("instance_id")});
+			view.instance.fetch({
+				success: function() {
+					// specific object type here
+					//
+					view.dialog = new Dialog({dialog_id: view.instance.get("object_id")});
+					view.dialog.fetch({
+						success: function() {
+							view.object_name = view.dialog.get("name");
+							view.render();
+						}
+					});
+				}
+			});
 		}
 
 		// get instance and object here, remove name from display
