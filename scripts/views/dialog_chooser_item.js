1ce3fafbabef9fe297c0cccafff7a0580873da3e
Kevin Alford <zeroeth@pixelflow.org>
2014-05-24 16:37:06 -0500

    Create from existing dialog

--- scripts/views/dialog_chooser_item.js
+++ scripts/views/dialog_chooser_item.js
@@ -1,8 +1,11 @@
 define([
 	'backbone',
 	'text!../../templates/dialog_chooser_item.tpl',
+	'models/trigger',
+	'models/instance',
+	'views/dialog_trigger_editor',
 	'vent'
-], function(Backbone, Template, vent) {
+], function(Backbone, Template, Trigger, Instance, DialogTriggerEditorView, vent) {
 
 	return Backbone.Marionette.ItemView.extend({
 		template: _.template(Template),
@@ -18,10 +21,11 @@ define([
 
 		// TODO how to bubble up? or get scene passed to us
 		onClickNewInstance: function() {
-			//var dialog_instance = new DialogInstance({game_id: this.model.get("game_id")});
+			var trigger  = new Trigger  ({game_id: this.options.parent.get("game_id"), scene_id: this.options.parent.get("scene_id")});
+			var instance = new Instance ({game_id: this.options.parent.get("game_id")});
 
-			//var dialog_creator  = new DialogCreatorView({scene: this.options.parent, dialog: this.model, dialog_instance: dialog_instance});
-			//vent.trigger("application:info:show", dialog_creator);
+			var trigger_editor = new DialogTriggerEditorView({scene: this.options.parent, dialog: this.model, instance: instance, model: trigger});
+			vent.trigger("application:info:show", trigger_editor);
 		}
 	});
 });
