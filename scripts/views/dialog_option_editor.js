5069e9e361747370d9cb9a7d1730567887e2b3c9
Kevin Alford <zeroeth@pixelflow.org>
2014-08-21 17:07:43 -0500

    Requirements for options

--- scripts/views/dialog_option_editor.js
+++ scripts/views/dialog_option_editor.js
@@ -2,8 +2,33 @@ define([
 	'underscore',
 	'backbone',
 	'text!templates/dialog_option_editor.tpl',
+	'views/requirements',
+	'models/requirement_package',
+	'models/game',
+	'collections/and_packages',
+	'collections/atoms',
+	'collections/items',
+	'collections/tags',
+	'collections/plaques',
+	'collections/dialogs',
+	'collections/web_pages',
+	'collections/quests',
+	'collections/web_hooks',
 	'vent'
-], function(_, Backbone, Template, vent) {
+], function(_, Backbone, Template,
+	RequirementsEditorView,
+	RequirementPackage,
+	Game,
+	AndPackagesCollection,
+	AtomsCollection,
+	ItemsCollection,
+	TagsCollection,
+	PlaquesCollection,
+	DialogsCollection,
+	WebPagesCollection,
+	QuestsCollection,
+	WebHooksCollection,
+	vent) {
 	return Backbone.Marionette.ItemView.extend({
 
 		template: _.template(Template),
@@ -108,6 +133,54 @@ define([
 		onClickCancel: function() {
 			this.model.set(this.previous_attributes);
 			vent.trigger("application:info:hide");
+		},
+
+		onClickEditRequirements: function() {
+			var view = this;
+
+			var requirement_package = new RequirementPackage({requirement_root_package_id: view.model.get("requirement_root_package_id"), game_id: view.model.get("game_id")});
+
+			var game = new Game({game_id: view.model.get("game_id")});
+
+			var contents = {
+				items:      new ItemsCollection    ([], {parent: game}),
+				tags:       new TagsCollection     ([], {parent: game}),
+				plaques:    new PlaquesCollection  ([], {parent: game}),
+				dialogs:    new DialogsCollection  ([], {parent: game}),
+				web_pages:  new WebPagesCollection ([], {parent: game}),
+				quests:     new QuestsCollection   ([], {parent: game}),
+				hooks:      new WebHooksCollection ([], {parent: game})
+			};
+
+			if(requirement_package.id === "0") { requirement_package.fetch = function() {}; }
+
+			$.when(contents.items.fetch(), contents.tags.fetch(), contents.plaques.fetch(), contents.dialogs.fetch(), contents.web_pages.fetch(), contents.quests.fetch(), contents.hooks.fetch(), requirement_package.fetch()).done(function()
+			{
+				// Load associations into collections
+				var and_packages = new AndPackagesCollection(requirement_package.get("and_packages"));
+				requirement_package.set("and_packages", and_packages);
+
+				and_packages.each(function(and_package) {
+					var atoms = new AtomsCollection(and_package.get("atoms"));
+					and_package.set("atoms", atoms);
+				});
+
+				// launch editor
+				var requirements_editor = new RequirementsEditorView({model: requirement_package, collection: and_packages, contents: contents});
+
+				requirements_editor.on("cancel", function()
+				{
+					vent.trigger("application:popup:hide");
+				});
+
+				requirements_editor.on("requirement_package:save", function(requirement_package)
+				{
+					view.model.set("requirement_root_package_id", requirement_package.id);
+					vent.trigger("application:popup:hide");
+				});
+
+				vent.trigger("application:popup:show", requirements_editor, "Locks Editor");
+			});
 		}
 	});
 });
