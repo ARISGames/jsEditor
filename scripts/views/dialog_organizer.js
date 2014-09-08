ec6207e606cf0229f197fdc4fd6812848700f5e9
Kevin Alford <zeroeth@pixelflow.org>
2014-09-08 12:06:44 -0500

    Create dialogs from sidebar

--- scripts/views/dialog_organizer.js
+++ scripts/views/dialog_organizer.js
@@ -2,8 +2,11 @@ define([
        'backbone',
        'text!templates/dialog_organizer.tpl',
        'views/dialog_organizer_row',
+       'views/dialog_editor',
+	   'models/dialog',
+	   'models/media',
        'vent'
-], function(Backbone, Template, DialogsOrganizerRowView, vent) {
+], function(Backbone, Template, DialogsOrganizerRowView, DialogEditorView, Dialog, Media, vent) {
 
 	return Backbone.Marionette.CompositeView.extend({
 		template: _.template(Template),
@@ -16,6 +19,22 @@ define([
 
 			vent.on("dialog:add", function(dialog) {
 				view.collection.add(dialog);
+			});
+		},
+
+		events: {
+			"click .new": "onClickNew"
+		},
+
+
+		onClickNew: function() {
+			var dialog = new Dialog({game_id: this.model.get("game_id")});
+
+			var icon   = new Media({media_id: dialog.get("icon_media_id")});
+
+			$.when(icon.fetch()).done(function () {
+				var dialog_editor = new DialogEditorView({model: dialog, icon: icon});
+				vent.trigger("application:popup:show", dialog_editor, "Create Conversation");
 			});
 		}
 	});
--- templates/item_organizer.tpl
+++ templates/item_organizer.tpl
@@ -1,6 +1,8 @@
 <table class="table table-hover">
 	<thead>
-		<th class="text-muted"><span class="glyphicon glyphicon-stop"></span> Items & Player Attributes
+		<th class="text-muted"><span class="glyphicon glyphicon-stop"></span>
+			Items & Player Attributes
+
 			<div class="pull-right">
 				<a class="new">
 					<span class="glyphicon glyphicon-plus-sign"></span>
