91a9f0abc3dc5a6958b1062a5b2b951e83266fca
Kevin Alford <zeroeth@pixelflow.org>
2014-11-11 15:22:58 -0600

    Tags have default media [#77656216]

--- scripts/views/tag_row.js
+++ scripts/views/tag_row.js
@@ -1,11 +1,12 @@
-define([
-	'underscore',
-	'backbone',
-	'text!templates/tag_row.tpl',
-	'views/tag_editor',
-	'models/media',
-	'vent'
-], function(_, Backbone, Template, TagEditorView, Media, vent) {
+define(function(require)
+{
+	var _             = require('underscore');
+	var Backbone      = require('backbone');
+	var Template      = require('text!templates/tag_row.tpl');
+	var TagEditorView = require('views/tag_editor');
+	var vent          = require('vent');
+
+
 	return Backbone.Marionette.ItemView.extend({
 		template: _.template(Template),
 
@@ -22,15 +23,8 @@ define([
 		},
 
 		onClickEdit: function() {
-			var view = this;
-
-			var media = new Media({media_id: this.model.get("media_id")});
-
-			$.when(media.fetch()).done(function()
-			{
-				var tag_editor = new TagEditorView({model: view.model, media: media});
-				vent.trigger("application:popup:show", tag_editor, "Edit Tag");
-			});
+			var tag_editor = new TagEditorView({model: this.model});
+			vent.trigger("application:popup:show", tag_editor, "Edit Tag");
 		}
 
 	});
