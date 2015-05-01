3b54530968a9c306f0bd63e33faec93857a14d77
Kevin Alford <zeroeth@pixelflow.org>
2015-05-01 11:05:53 -0500

    Sanitize descriptions

--- scripts/views/quest_row.js
+++ scripts/views/quest_row.js
@@ -9,6 +9,12 @@ define(function(require)
 	return Backbone.Marionette.ItemView.extend({
 		template: _.template(Template),
 
+		templateHelpers: function() {
+			return {
+				sanitize_html: this.sanitize_html
+			}
+		},
+
 		// Bootstrap
 		tagName: 'a',
 		className: "list-group-item",
@@ -26,6 +32,33 @@ define(function(require)
 
 			var quest_editor = new QuestEditorView({model: view.model});
 			vent.trigger("application:popup:show", quest_editor, "Edit Quest");
+		},
+
+		sanitize_html: function(html) {
+			// Attempt 1
+			//html.replace(/(<([^>]+)>)/ig,"")
+
+			// Attempt 2
+			//var tmp = document.createElement("DIV");
+			//tmp.innerHTML = html;
+			//return tmp.textContent || tmp.innerText;
+
+			// Attempt 3
+			var div = document.createElement('div');
+			div.innerHTML = html;
+			var scripts = div.getElementsByTagName('script');
+			var i = scripts.length;
+			while (i--) {
+			  scripts[i].parentNode.removeChild(scripts[i]);
+			}
+
+			var styles = div.getElementsByTagName('style');
+			var i = styles.length;
+			while (i--) {
+			  styles[i].parentNode.removeChild(styles[i]);
+			}
+
+			return div.textContent || div.innerText;
 		}
 
 	});
