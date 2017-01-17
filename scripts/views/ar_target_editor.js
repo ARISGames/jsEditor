f6c4aab2a6d42a1fe134322f7831aa41a9f0d88c
Phildo <Phildo211@gmail.com>
2017-01-17 14:03:23 -0600

    ar view refreshes after upload

--- scripts/views/ar_target_editor.js
+++ scripts/views/ar_target_editor.js
@@ -32,19 +32,15 @@ function(
     events:
     {
       "click .upload": "onClickUpload",
-      "dragenter .ar_target-drop-target": "onDragEnter",
-      "dragleave .ar_target-drop-target": "onDragLeave",
-      "dragend .ar_target-drop-target": "onDragEnd",
-      "dragover .ar_target-drop-target": "onDragOver",
-      "drop .ar_target-drop-target": "onDrop",
     },
 
     initialize: function()
     {
-      var view = this;
-
-      this.reader = new FileReader();
-      this.reader.onload = this.onReadFile.bind(this);
+      vent.on("ar_target:upload", function(ar_target)
+      {
+        this.render();
+        //window.location.reload();
+      });
     },
 
     onRender: function()
@@ -57,60 +53,6 @@ function(
       vent.trigger("application:popup:show", new ARTargetUploadView({model: ar_target_db}), "Upload ARTarget");
     },
 
-
-    onDragEnter: function(event)
-    {
-      this.$el.find(".ar_target-drop-target").addClass("ar_target-hover");
-      event.preventDefault();
-      return false;
-    },
-
-    onDragOver: function(event)
-    {
-      this.$el.find(".ar_target-drop-target").addClass("ar_target-hover");
-      event.preventDefault();
-      return false;
-    },
-
-    onDragEnd: function(event)
-    {
-      this.$el.find(".ar_target-drop-target").addClass("ar_target-hover");
-      event.preventDefault();
-      return false;
-    },
-
-    onDragLeave: function(event)
-    {
-      this.$el.find(".ar_target-drop-target").removeClass("ar_target-hover");
-      event.preventDefault();
-      return false;
-    },
-
-    onDrop: function(event)
-    {
-      this.$el.find(".ar_target-drop-target").removeClass("ar_target-hover");
-      event.preventDefault();
-
-      var file = event.originalEvent.dataTransfer.files[0];
-
-      this.reader.readAsDataURL(file);
-
-      return false;
-    },
-
-    // TODO or pop up editor? single vs multiple file workflow.
-    onReadFile: function(event)
-    {
-      var view = this;
-
-      var data = event.target.result;
-
-      // strip base64 header
-      var start = data.indexOf(",") + 1;
-      var data  = data.substr(start);
-
-      vent.trigger("ar_target:upload", view.model);
-    }
   });
 });
 
